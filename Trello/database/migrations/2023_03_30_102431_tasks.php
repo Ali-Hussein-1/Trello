65bb565a21293c3525f6e7223632401e1abc7268
Ali-Hussein-1 <ali.kh.hussein@gmail.com>
2023-03-30 14:10:14 +0300

    Editing the migrations

--- Trello/database/migrations/2023_03_30_102431_tasks.php
+++ Trello/database/migrations/2023_03_30_102431_tasks.php
@@ -11,7 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        //
+        
+
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->timestamps();
+        });
     }
 
     /**
