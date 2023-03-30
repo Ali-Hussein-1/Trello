4b9a70a9694a89b246a84e09c20e7bf229cd17e9
Ali-Hussein-1 <ali.kh.hussein@gmail.com>
2023-03-30 14:14:11 +0300

    Editing the categories table

--- Trello/database/migrations/2023_03_30_102343_categories.php
+++ Trello/database/migrations/2023_03_30_102343_categories.php
@@ -14,7 +14,9 @@ return new class extends Migration
         
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('board_id')->constrained();
             $table->string('name');
+            $table->string('psition');
             $table->timestamps();
         });
     }
