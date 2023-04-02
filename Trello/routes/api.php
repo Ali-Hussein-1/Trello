80a10b4f80fbaec46fbba5c3c595efbc030bca63
allawhussein <allawhussein@gmail.com>
2023-04-02 15:37:12 +0300

    added create board controller route

--- Trello/routes/api.php
+++ Trello/routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\BoardController;
 
 Route::controller(AuthController::class)->group(function () {
     Route::post('login', 'login');
@@ -10,6 +11,10 @@ Route::controller(AuthController::class)->group(function () {
     Route::post('logout', 'logout');
     Route::post('refresh', 'refresh');
     Route::get('me', 'me');
+});
+
+Route::controller(BoardController::class)->group(function (){
+    Route::post('create', 'create');
 });
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
