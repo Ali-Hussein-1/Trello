de2003cc2d39a2ebbc4d2e45314c8a37f616379c
allawhussein <allawhussein@gmail.com>
2023-04-02 13:56:54 +0300

    migration update

--- Trello/database/migrations/2023_03_30_102423_comments.php
+++ Trello/database/migrations/2023_03_30_102423_comments.php
@@ -13,8 +13,8 @@ return new class extends Migration
     {
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('task_id');
-            $table->foreignId('user_id')->constrained();
+            $table->foreignId('user_id')->references('id')->on('users');
+            $table->foreignId('task_id')->references('id')->on('tasks');
             $table->string('content');
             $table->timestamps();
         });
